64ce9bb76b00277da417d657469b77cc1faa9c65
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-02-02 14:03:33 -0600

    add issue management components and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'issues',
+    loadComponent: () => import('./modules/issues/pages/issue-list/issue-list-page.component').then(m => m.default)
+  },
+  {
+    path: 'issue/:number',
+    loadComponent: () => import('./modules/issues/pages/issue/issue-page.component').then(m => m.default)
+  },
+  {
+    path: '**',
+    redirectTo: 'issues',
+  }
+];
---
+++ src/app/modules/issues/pages/issue-list/issue-list-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-list-page',
+  imports: [],
+  templateUrl: './issue-list-page.component.html',
+})
+export default class IssueListPageComponent { }
