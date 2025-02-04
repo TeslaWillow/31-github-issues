9213231e1f44d67fc8af503ed25ebb737176c640
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-02-03 20:26:41 -0600

    add labels selector component to issue list page

--- src/app/modules/issues/pages/issue-list/issue-list-page.component.ts
+++ src/app/modules/issues/pages/issue-list/issue-list-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { IssuesService } from '../../services/issues.service';
 import { CommonModule } from '@angular/common';
+import { LabelsSelectorComponent } from '../../components/labels-selector/labels-selector.component';
 
 @Component({
   selector: 'app-issue-list-page',
-  imports: [ CommonModule ],
+  imports: [ CommonModule, LabelsSelectorComponent ],
   templateUrl: './issue-list-page.component.html',
 })
 export default class IssueListPageComponent {
