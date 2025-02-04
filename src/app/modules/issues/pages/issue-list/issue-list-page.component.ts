f5a53a3d940df005620b33484353cdd87149ccfe
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-02-03 19:48:23 -0600

    get data from tanstack query.

--- src/app/modules/issues/pages/issue-list/issue-list-page.component.ts
+++ src/app/modules/issues/pages/issue-list/issue-list-page.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IssuesService } from '../../services/issues.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-issue-list-page',
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './issue-list-page.component.html',
 })
-export default class IssueListPageComponent { }
+export default class IssueListPageComponent {
+
+  private issuesService = inject(IssuesService);
+
+  get labelsQuery() {
+    return this.issuesService.labelsQuery;
+  }
+
+}
